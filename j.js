1bccf58b5b2c2c32cf531c5b9f56eb77ec8ce1e2
Tiberiu-Arthur Nowotny <thundersocks69@gmail.com>
2020-12-19 17:19:32 +0100

    Added XSS script

--- j.js
+++ j.js
@@ -1,6 +1,20 @@
-start_me_senpai()
-
+//start_me_senpai()
+lePrank();
 function start_me_senpai(){
 alert("You have been gnomed :DDD");
 window.open("https://google.at/","_self");
+}
+
+function lePrank(){
+
+
+    alert("I have a dream");
+    alert("That one day");
+    alert("I'll be able to fly");
+    alert("to sing");
+    alert("to explore the universe");
+    alert("And to escape this nightmare");
+
+
+
 }
